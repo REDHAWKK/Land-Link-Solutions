472a60e2f9c4c25982be208cb84fbae67d65eead
REDHAWKK <Princefrosh2007@gmail.com>
2026-05-17 20:12:33 -0700

    .

--- Js/properties.js
+++ Js/properties.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const properties = {
-    "apex-fields": {
-      title: "Apex Fields",
+    "land-1": {
+      title: "Land 1",
       location: "Obagie, Benin City",
       price: "₦45,000,000",
       size: "600 SQM",
@@ -9,13 +9,13 @@ document.addEventListener("DOMContentLoaded", () => {
       titleType: "C of O",
       topography: "Dry Land",
       description:
-        "Apex Fields is a dry, well-drained land located in a fast-developing area. Ideal for residential homes and long-term investment.",
+        "Land 1 is a dry, well-drained land located in a fast-developing area. Ideal for residential homes and long-term investment.",
       video: "Videos/land1.mp4",
       thumbnail: "Images/land1.png"
     },
 
-    "heritage-acres": {
-      title: "Heritage Acres",
+    "land-2": {
+      title: "Land 2",
       location: "NIFOR, Benin City",
       price: "₦47,000,000",
       size: "10,000 SQM",
@@ -23,12 +23,12 @@ document.addEventListener("DOMContentLoaded", () => {
       titleType: "C of O",
       topography: "Dry Land",
       description:
-        "NIFOR is a dry, well-drained land located in a fast-developing area. Ideal for residential homes and long-term investment.",
+        "Land 2 is a dry, well-drained land located in a fast-developing area. Ideal for residential homes and long-term investment.",
       video: "Videos/land1.mp4",
       thumbnail: "Images/land2.png"
     },
-    "Commercial-Hub-Land": {
-      title: "Commercial Hub Land",
+    "land-3": {
+      title: "Land 3",
       location: "Evbuowe, Airport Road Axis, Benin City",
       price: "₦10,000,000",
       size: "465 SQM",
@@ -36,9 +36,22 @@ document.addEventListener("DOMContentLoaded", () => {
       titleType: "C of O",
       topography: "Dry Land",
       description:
-        "Commercial Hub Land is a dry, well-drained land located in a fast-developing area. Ideal for commercial ventures and long-term investment.",
+        "Land 3 is a dry, well-drained land located in a fast-developing area. Ideal for commercial ventures and long-term investment.",
       video: "Videos/land1.mp4",
       thumbnail: "Images/land3.png"
+    },
+    "land-4": {
+      title: "Land 4",
+      location: "Evbuowe, Airport Road Axis, Benin City",
+      price: "₦10,000,000",
+      size: "465 SQM",
+      use: "Commercial",
+      titleType: "C of O",
+      topography: "Dry Land",
+      description:
+        "Land 4 is a dry, well-drained land located in a fast-developing area. Ideal for commercial ventures and long-term investment.",
+      video: "Videos/land1.mp4",
+      thumbnail: "Images/land4.png"
     }
 
   };
@@ -127,7 +140,7 @@ document.addEventListener("DOMContentLoaded", () => {
   window.playVideo = playVideo;
 
 });
-function animateCount(id, target, duration = 2000) {
+function animateCount(id, target, duration = 1000) {
   const element = document.getElementById(id);
 
   let start = 0;
@@ -150,5 +163,5 @@ function animateCount(id, target, duration = 2000) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  animateCount("land-count", 20);
+  animateCount("land-count", 6);
 });
